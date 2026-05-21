683eb83d35de0f3a09efb29c9d531dcdc45e0aee
Abdullah <smsm17555@gmail.com>
2026-05-21 16:00:15 +0300

    Switch dashboard to email password login
    # Conflicts:
    #	src/App.css
    #	src/App.jsx
    #	src/firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwRPc6JCurJMa2-verO3XLJxYqpd3l-Mg",
+  authDomain: "iam-dashboard-1755.firebaseapp.com",
+  projectId: "iam-dashboard-1755",
+  storageBucket: "iam-dashboard-1755.firebasestorage.app",
+  messagingSenderId: "960424171655",
+  appId: "1:960424171655:web:d60dbaecdf440b90ed4022",
+  measurementId: "G-R4982HLCL9"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
